c5df00452d0357e453bb4a75b55a2a2bbac9bc45
steffanynaranjov <1792@holbertonschool.com>
2020-08-19 04:22:47 +0000

    header 1.1

---
+++ shell.h
@@ -0,0 +1,52 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include<string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+/**
+ * struct args - arguments
+ * @argc: numbers of argv
+ * @argv: double pointer of strings
+ *
+ * Description: struct arguments
+ */
+typedef struct args
+{
+	int argc;
+	char **argv;
+} args_t;
+/**
+ * struct run_f - Strucrun in
+ * @fc: name function
+ * @f: function direction
+ */
+typedef struct run_f
+{
+	char *fc;
+	int (*f)(char **argv);
+} run_t;
+
+int main(int argc, char **argv, char **env);
+int exec(char **args);
+char *gline(void);
+void handle_sigint(int n);
+void free_memory(char **list, int t);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _print_env(char *av, char **env);
+int _exitt(char *av, int num);
+void _print_error(int status, char *s);
+args_t split_line(char *line);
+
+#endif /* SHELL_H */
