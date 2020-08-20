e24130918d1d449f834829ba9c38a9764255c72d
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-08-19 22:27:58 -0500

    add printenv func

---
+++ print_env.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+/**
+* print_env - environment variables
+* @av: command to recibe
+* @env: all environment variables for the user
+*
+* Description: prints all environment variables
+* Return: 1
+*/
+int print_env(char *av, char **env)
+{
+	unsigned int x = 0, length;
+	int status;
+	status = _strcmp(av, "env");
+
+	if (status == 0)
+	{
+		x = 0;
+		while (env[x])
+		{
+			length = _strlen(env[x]);
+			write(STDOUT_FILENO, env[x], length);
+			write(STDOUT_FILENO, "\n", 1);
+			++x;
+		}
+	}
+	return (1);
+}
