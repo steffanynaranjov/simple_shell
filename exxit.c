f3b2a8aca6db878cc3781229da8be722fe43a491
steffanynaranjov <1792@holbertonschool.com>
2020-08-20 03:03:03 +0000

    exit shell

---
+++ exxit.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+* exxitt - handle exit command
+* @av: string
+* @num: num
+* Description: out of the shell with exit
+* Return: 1 0r 0
+*/
+int exxit(char *av, int num)
+{
+	int status;
+	status = _strcmp(av, "exit");
+
+	if (status == 0)
+	{
+		free(av);
+		if (num == 512) /*exit 2 */
+			exit(2);
+		else
+			exit(num);
+	}
+	return (1);
+}
